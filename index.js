dee09726c1488ecf14dea079ff8d0f9f981d099a
abtirish <abtirish@gmail.com>
2023-09-27 15:25:24 -0600

    finish phase 0 the dom modifying elements lab. this one was hard!

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+const node = document.getElementById("main");
+if (node.parentNode) {
+    node.parentNode.removeChild(node);
+}
+let newHeader;
+
+document.body.onload = addElement;
+
+function addElement() {
+  newHeader = document.createElement("h1");
+  newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Amanda is the champion";
+
+
+  const currentDiv = document.getElementById("script");
+  document.body.insertBefore(newHeader, currentDiv);
+}
+
